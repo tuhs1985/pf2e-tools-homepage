c8e9656e5d74a73473d28f2ecbee84363b6b3281
tuhs1985 <77306309+tuhs1985@users.noreply.github.com>
2026-02-08 16:27:53 -0500

    temporarily removed tableplop exporter from list

--- src/App.tsx
+++ src/App.tsx
@@ -19,19 +19,27 @@ export default function App() {
             >
               Crafting Tool
             </a>
-			            <a
-              className="hub-btn"
-              href="https://pb2tp.tuhsrpg.com"
-            >
-              Pathbuilder 2e to Tableplop exporter
-            </a>
+
           </div>
-          <footer>
+        </div>
+          <footer className="footer">
             <a href="https://github.com/tuhs1985/pf2e-tools-homepage">
               View on GitHub / Report Issues
             </a>
+			          
+          <p className="paizo-notice">
+            This website uses trademarks and/or copyrights owned by Paizo Inc., used under Paizo's Community Use Policy (paizo.com/licenses/communityuse). 
+            We are expressly prohibited from charging you to use or access this content. This website is not published, endorsed, or specifically approved by Paizo. 
+            For more information about Paizo Inc. and Paizo products, visit{' '}
+            <a 
+              href="https://paizo.com/" 
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              paizo.com
+            </a>.
+          </p>
           </footer>
-        </div>
       </div>
     </div>
   );
